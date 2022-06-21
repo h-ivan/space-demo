005adc3d34fb43e86607be9f3d7aa33dd2e48871
ivan77n <h649477@gmail.com>
2022-06-21 20:51:54 -0300

    technology section complete

--- src/components/Technology.jsx
+++ src/components/Technology.jsx
@@ -1,42 +1,64 @@
 import {NavLink, useParams} from "react-router-dom";
 import technology from '../technology.json';
+import {useEffect, useState} from "react";
 
 function Technology() {
+
     const url = useParams();
     const tech = technology.filter(e => e.url === url['vehicle'])[0];
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+    const imageUrl = windowWidth >= 940 ? '' : '-sm';
+
     const techLinks = technology.map((e) => (
         <li key={e.id} className="nav-item">
-            <NavLink  className="hover-underline-animation" to={`/technology/${e.url}`}> {e.id} </NavLink>
+            <NavLink className={({ isActive }) =>
+                (isActive ? "explore-tech active-tech" : "explore-tech")}
+                     to={`/technology/${e.url}`}>
+                <span>{e.id}</span>
+            </NavLink>
         </li>
     ));
 
+    useEffect(() => {
+        const handleWindowResize = () => {
+            setWindowWidth(window.innerWidth);
+        };
+        window.addEventListener('resize', handleWindowResize);
+        return () => {
+            window.removeEventListener('resize', handleWindowResize);
+        }
+    }, []);
+
     function getImageUrl(img) {
-        return new URL(`../assets/img/${img}`, import.meta.url).href
+        return new URL(`../assets/img/${img+imageUrl}.jpg`, import.meta.url).href;
     }
 
     return (
         <div>
             <div className="bg-image bg-technology kenburns-bottom-right"/>
-            <div className="destination-wrapper">
+            <div className="heading-5 tech-title">
+                <span className="destination-heading-span">02</span> space launch 101
+            </div>
+            <div className="tech-wrapper">
                 <div className="destination-planet-container">
-                    <div className="heading-5 destination-title">
-                        <span className="destination-heading-span">02</span> space launch 101
-                    </div>
-                    <img className="planet-img" src={getImageUrl(tech.img)} width="515" height="527"
-                         alt="moon"></img>
-                </div>
-                <div className="planet-content">
-                    <div className="planet-menu">
-                        <ul className="planet-nav-content">
-                            {techLinks}
-                        </ul>
-                    </div>
-                    <div>
-                        <div className="heading-1 planet">{tech.title}</div>
-                        <div className="body-text">
-                            {tech.description}
+                    <div className="tech-container">
+                        <div>
+                            <ul className="tech-nav-content">
+                                {techLinks}
+                            </ul>
+                        </div>
+                        <div>
+                            <div className="body-text-condensed">THE TERMINOLOGY…</div>
+                            <div className="heading-3">{tech.title}</div>
+                            <div className="body-text">
+                                {tech.description}
+                            </div>
                         </div>
                     </div>
+                </div>
+                <div className="tech-content">
+                    <img className="tech-img" src={getImageUrl(tech.img)} width="515" height="527"
+                         alt="moon"></img>
                 </div>
             </div>
         </div>
